7e8d4f80685badcf4df3174a98b2229db4cc96cb
Yumn Teshome <59879314+Yumn-Teshome@users.noreply.github.com>
2023-06-07 14:04:39 -0500

    Update layers.py
    remove .to() on PolyNeurons

--- block/nn/layers.py
+++ block/nn/layers.py
@@ -144,9 +144,9 @@ class PolyNeurons(BaseNeurons):
     def _to_current(self, x):
         return (self.fc2(x)+1) * self.fc1(x)
     
-    def to(self, device):
+"""    def to(self, device):
         self.fc2.to(device)
-        self.fc1.to(device)
+        self.fc1.to(device)"""
 
     def forward(self, x, v_init=None, return_type=methods.RETURN_SPIKES):
 
